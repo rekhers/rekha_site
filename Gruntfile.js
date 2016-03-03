9893008b0b3ffe01cb81e7f23798d02e24af86e1
Rekha Tenjarla <Rekha@Rekhas-MacBook-Pro.local>
2016-03-02 22:53:31 -0800

    auto compiling less

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,31 +1,51 @@
-
-
 module.exports = function(grunt) {
-  require('jit-grunt')(grunt);
-
-  grunt.initConfig({
-    less: {
-      development: {
-        options: {
-          compress: true,
-          yuicompress: true,
-          optimization: 2
+    grunt.initConfig({
+        less: {
+            development: {
+                options: {
+                    paths: ["./public/stylesheets"],
+                    yuicompress: true
+                },
+                files: {
+                    "public/stylesheets/home.css": "public/stylesheets/home.less"
+                }
+            }
         },
-        files: {
-          "public/stylesheets/home.css": "public/stylesheets/home.less" // destination file and source file
+        watch: {
+            files: "public/stylesheets/*",
+            tasks: ["less"]
         }
-      }
-    },
-    watch: {
-      styles: {
-        files: ['less/**/*.less'], // which files to watch
-        tasks: ['less'],
-        options: {
-          nospawn: true
-        }
-      }
-    }
-  });
-
-  grunt.registerTask('default', ['less', 'watch']);
+    });
+    grunt.loadNpmTasks('grunt-contrib-less');
+    grunt.loadNpmTasks('grunt-contrib-less');
 };
+
+// module.exports = function(grunt) {
+//   require('jit-grunt')(grunt);
+
+//   grunt.initConfig({
+//     less: {
+//       development: {
+//         options: {
+//           compress: true,
+//           yuicompress: true,
+//           optimization: 2
+//         },
+//         files: {
+//           "public/stylesheets/home.css": "public/stylesheets/home.less" // destination file and source file
+//         }
+//       }
+//     },
+//     watch: {
+//       styles: {
+//         files: ['less/**/*.less'], // which files to watch
+//         tasks: ['less'],
+//         options: {
+//           nospawn: true
+//         }
+//       }
+//     }
+//   });
+
+//   grunt.registerTask('default', ['less', 'watch']);
+// };
